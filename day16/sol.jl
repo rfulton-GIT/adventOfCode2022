011654ee5057299272227f770c5250b95bd6f477
rmfulton <rmfulton@gmail.com>
2022-12-26 10:07:22 -0800

    changed the for-loop structure of julia files

--- day16/sol.jl
+++ day16/sol.jl
@@ -134,41 +134,41 @@ function get_answer(time_limit, g, ordering, net_pressure)
     end
     return DP[1][1][index(ordering, "AA")]
 end
-println()
-print("Singly Threaded: ")
-@time println(get_answer(30, g, ordering, pressure))
+# println()
+# print("Singly Threaded: ")
+# @time println(get_answer(30, g, ordering, pressure))
 
 function multithreaded(time_limit, g, ordering, net_pressure)
     L = length(g)
     subsets = 2^L
-    DP = [[[0 for position in 1:L] for config in 1:subsets ] for minute in 1:time_limit+1]
+    DP = [[0 for pos_and_config in 0:(L*subsets - 1)] for minute in 1:time_limit+1]
     # it must be backwards for optimality
     # this takes 30*L*(2**L)
     for minute in time_limit:-1:1
-        Threads.@threads for config in 0:subsets - 1
-            for position in 1:L
-                initial_benefit = net_pressure[config+1]
-                node = g[ordering[position]]
-                isOff = (config >>(position-1)) % 2 == 0
-                turnOn = DP[minute+1][config + isOff*2^(position-1) + 1][position]
-                best = turnOn + initial_benefit
-                for pair in node.adj
-                    name,pos = pair
-                    new_pos = index(ordering, name)
-                    travel_time = node.adj[name]
-                    if travel_time + minute > time_limit
-                        best = max(best, initial_benefit)
-                    else
-                        alt = travel_time*initial_benefit 
-                        alt += DP[minute + travel_time][config+1][new_pos]
-                        best = max(best, alt)
-                    end
+        Threads.@threads for pos_and_config in 0:(L*subsets - 1)
+            config = pos_and_config÷L 
+            position = (pos_and_config % L) + 1
+            initial_benefit = net_pressure[config+1]
+            node = g[ordering[position]]
+            isOff = (config >>(position-1)) % 2 == 0
+            turnOn = DP[minute+1][position + L*(config + isOff*2^(position-1)) ]
+            best = turnOn + initial_benefit
+            for pair in node.adj
+                name,pos = pair
+                new_pos = index(ordering, name)
+                travel_time = node.adj[name]
+                if travel_time + minute > time_limit
+                    best = max(best, initial_benefit)
+                else
+                    alt = travel_time*initial_benefit 
+                    alt += DP[minute + travel_time][new_pos + config*L]
+                    best = max(best, alt)
                 end
-                DP[minute][config+1][position] = best
             end
+            DP[minute][position + config*L] = best
         end
     end
-    return DP[1][1][index(ordering, "AA")]
+    return DP[1][index(ordering, "AA")]
 end
 
 println()
